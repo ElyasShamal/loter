8a9160048ac39f4b3d260625bb48b70699b94c4c
Elyas <Elyas.shamal2@gmail.com>
2023-10-20 10:05:18 -0400

    create wrapper sections and add background images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import textContent from "./textContents";
 
-import images from "..image-1.jpg";
+import image1 from "../images/image-1.jpg";
+import image2 from "../images/image-2.jpg";
+import image3 from "../images/image-3.jpg";
+import image4 from "../images/image-4.jpg";
 
 function Home() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [currentText, setCurrentText] = useState(0);
+  const images = [image1, image2, image3, image4];
+  const texts = textContent;
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+      setCurrentText((prevText) => (prevText + 1) % texts.length);
+    }, 18000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  });
+
   return (
     <main className="main-container">
-      <img src={images} alt="background" />
+      <div className="background-image">
+        <img className="image" src={images[currentImage]} alt="background" />
+        <div className="advertisment">
+          <p>{texts[currentText]}</p>
+          <button>Shop Now</button>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/components/Home/textContents.js
@@ -0,0 +1,6 @@
+const textContent = [
+  "Our Popular Products",
+  "We Provide You Super Quality Shoes",
+  "Special Offer",
+];
+export default textContent;
